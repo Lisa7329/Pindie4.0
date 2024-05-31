9ed71fd797b8ef3a606ff5d3a15b9b687ae92d6e
女ね子 <mr.doggy666@mail.ru>
2024-05-31 07:10:44 +0300

    pindie.yourdomain.nomorepartiesco.ru в cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,7 +2,8 @@ const allowedCors = [
     'https://practicum.yandex.ru',
     'https://students-projects.ru',
     'http://localhost:3000',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'pindie.yourdomain.nomorepartiesco.ru'
 ];
 
 function cors(req, res, next) {
